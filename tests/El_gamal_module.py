445458e7ea8e6c12b2026c5e8e9f3629a60d1bb5
unknown <soukainabouhayoufi6@gmail.com>
2025-12-06 12:45:27 +0100

    Ajout du module ElGamal

---
+++ tests/El_gamal_module.py
@@ -0,0 +1,77 @@
+from Crypto.PublicKey import ElGamal
+from Crypto import Random
+from Crypto.Util.number import bytes_to_long, long_to_bytes
+import json
+import hashlib
+
+def generate_keypair(bits=2048):
+
+    key = ElGamal.generate(bits, Random.get_random_bytes)
+
+    public = {
+        "p": key.p,
+        "g": key.g,
+        "y": key.y
+    }
+
+    private = {
+        "p": key.p,
+        "g": key.g,
+        "x": key.x
+    }
+
+    return public, private
+
+
+
+def save_keys(prefix, public, private):
+
+    with open(f"{prefix}_public.json", "w") as f:
+        json.dump(public, f)
+
+    with open(f"{prefix}_private.json", "w") as f:
+        json.dump(private, f)
+
+
+def load_keys(prefix):
+
+    with open(f"{prefix}_public.json", "r") as f:
+        pub = json.load(f)
+
+    with open(f"{prefix}_private.json", "r") as f:
+        priv = json.load(f)
+
+    return pub, priv
+
+
+def encrypt_aes_key(aes_key_bytes, public):
+
+    m = bytes_to_long(aes_key_bytes)
+
+    p = public["p"]
+    g = public["g"]
+    y = public["y"]
+
+    k = Random.random.StrongRandom().randint(1, p - 2)
+
+    a = pow(g, k, p)
+    b = (pow(y, k, p) * m) % p
+
+    return {"a": a, "b": b}
+
+
+
+def decrypt_aes_key(cipher, private):
+    a = cipher["a"]
+    b = cipher["b"]
+
+    p = private["p"]
+    x = private["x"]
+
+
+    s = pow(a, x, p)
+    s_inv = pow(s, -1, p)
+
+    m = (b * s_inv) % p
+
+    return long_to_bytes(m)
